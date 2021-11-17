4e24339dfbfcf21660e784f69229dacd16f29239
JacobTrevithick <82681667+JacobTrevithick@users.noreply.github.com>
2021-11-16 21:14:42 -0800

    Update app.js

--- static/js/app.js
+++ static/js/app.js
@@ -1,4 +1,4 @@
-const samplesPath = "Interactive-Web-Design-Challenge/data/samples.json";
+const samplesPath = "data/samples.json";
 
 function init() {
     // Initialize Hbar chart, bubble chart, demo info, gauge chart
